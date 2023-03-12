036b4e3db2c0a04d23309bc69e2b10677def7531
DeveloperDTH <scratchdth@gmail.com>
2023-03-12 10:49:15 +0900

    2023

--- public/js/mealapi.js
+++ public/js/mealapi.js
@@ -4,12 +4,17 @@ const Meal = async function (schoolname,schoolRegion,schoolGrade,mealDate) {
   // 학교 검색 및 첫 번째 결과의 학교 코드로 초기
     let result =  await school.search(School.Region[schoolRegion], schoolname);
     await school.init(School.Type[schoolGrade], School.Region[schoolRegion], result[0].schoolCode);
-    const [year, month, date] = mealDate.split('-');
+    const [year, month,date] = mealDate.split('-');
     const data = await school.getMeal(year,month);
-    let meal = data[date]
-    meal = meal.substr(5)
-    meal = meal.split(" ");
-    meal = meal[0]
+    let meal = data;
+    if (date != undefined){
+      meal = data[date]
+      // meal = meal.substr(5)
+      meal = meal.split(" ");
+      meal = meal[0] 
+      return meal;
+    }
     return meal;
 }
+
 module.exports = {Meal};
--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const PORT = 3000
 app.get('/', function (req, res) {
   res.send()
 })
-app.get("/:schoolName/:region/:grade/:date", async (req, res) => {
+app.get("/api/:schoolName/:region/:grade/:date", async (req, res) => {
   // let data=mealapi.schoolMeal(req.params.schoolName,req.params.region,req.params.grade,req.params.date);
-  let data = await Meal(req.params.schoolName,req.params.region,req.params.grade,req.params.date)
+  const data = await Meal(req.params.schoolName,req.params.region,req.params.grade,req.params.date)
+  // console.log(req.params.schoolName,req.params.region,req.params.grade,req.params.date)
   res.send(data);
 });
+// app.get("/:schoolName/:region/:grade/:date/:day", async (req, res) => {
+//   // let data=mealapi.schoolMeal(req.params.schoolName,req.params.region,req.params.grade,req.params.date);
+//   const data = await dateMeal(req.params.schoolName,req.params.region,req.params.grade,req.params.date,req.params.day  )
+//   res.send(data);
+// });
 
 app.listen(PORT,()=>{
     console.log("server start")
